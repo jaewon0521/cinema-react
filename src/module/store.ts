801a4d844085599df3acad071bcfdd775373f02a
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-11-19 23:03:18 +0900

    remove console.log

--- src/module/store.ts
+++ src/module/store.ts
@@ -1,22 +1,8 @@
 import { movieReducer } from "./reducers/movieSlice";
 import { errorReducer } from "./reducers/errorSlice";
-import { configureStore, createListenerMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
-import { changeMovieType, movieTypeReducer } from "./reducers/movieTypeSlice";
-
-const listenerMiddleware = createListenerMiddleware();
-
-listenerMiddleware.startListening({
-  actionCreator: changeMovieType,
-  effect: async (action, listenerApi) => {
-    console.log(action);
-    console.log(listenerApi);
-
-    await listenerApi.delay(5000);
-
-    console.log(listenerApi.getState());
-  },
-});
+import { movieTypeReducer } from "./reducers/movieTypeSlice";
 
 export const store = configureStore({
   reducer: {

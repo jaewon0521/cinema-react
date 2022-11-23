15060fcac1fdf55aef9a1d16e83afda61a77768c
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-11-23 13:16:09 +0900

    proxy setup

--- src/api/service.ts
+++ src/api/service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { MovieListResPonseType } from "../types/apiResponseType";
 
-const REQUEST_URL = "https://api.themoviedb.org/3";
+// const REQUEST_URL = "https://api.themoviedb.org/3";
 const REQUEST_LANGUAGE = "ko-KR";
 const API_KEY = process.env.REACT_APP_APP_THEMOVIEDB_KEY;
 
@@ -9,14 +9,14 @@ export const IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
 export const MOIVE_API_URL = async (type: string, page: number) => {
   const response = await axios.get<MovieListResPonseType>(
-    `${REQUEST_URL}/movie/${type}?api_key=${API_KEY}&language=${REQUEST_LANGUAGE}&page=${page}`
+    `/movie/${type}?api_key=${API_KEY}&language=${REQUEST_LANGUAGE}&page=${page}`
   );
   return response.data;
 };
 
 export const SEARCH_MOIVE_API_URL = async (query: string) => {
   const response = await axios.get<MovieListResPonseType>(
-    `${REQUEST_URL}/search/movie?api_key=${API_KEY}&language=${REQUEST_LANGUAGE}&query=${query}`
+    `/search/movie?api_key=${API_KEY}&language=${REQUEST_LANGUAGE}&query=${query}`
   );
   return response.data;
 };
